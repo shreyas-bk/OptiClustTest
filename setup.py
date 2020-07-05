11723518ebad0f13c6a872ac7e8e8fe3090ff13a
shreyas-bk <56465925+shreyas-bk@users.noreply.github.com>
2020-07-05 15:28:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="OptiClust",
+    name="OptiClustTest",
     version="0.0.2",
     author="Shreyas Kera",
     author_email="shreykera7@gmail.com",
     description="Python implementation of various algorithms to find the optimal number of clusters",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/shreyas-bk/OptiClust",
-    packages=['OptiClust'],
+    url="https://github.com/shreyas-bk/OptiClustTest",
+    packages=['OptiClustTest'],
     license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
