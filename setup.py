8449d3969557be6b32a52dbde1aef730ed630b3e
shreyas-bk <56465925+shreyas-bk@users.noreply.github.com>
2020-07-06 10:24:01 +0530

    v0.0.5 added pandas, ValueError, changed README

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="OptiClustTest",
-    version="0.0.4",
+    version="0.0.5",
     author="Shreyas Kera",
     author_email="shreykera7@gmail.com",
     description="Python implementation of various algorithms to find the optimal number of clusters",
